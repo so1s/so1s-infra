33a894687ea201845c2bf21b6ac15b5e2ae045fb
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-08-30 20:39:17 +0900

    refactor: separate provider, version file

--- live/global/main.tf
+++ live/global/main.tf
@@ -1,16 +1,3 @@
-provider "aws" {
-    region = "ap-northeast-2"
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.28.0"
-    }
-  }
-}
-
 resource "aws_s3_bucket" "remote_state" {
   bucket = "so1s-terraform-remote-state-storage"
   
