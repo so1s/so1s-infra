d19fd8e79b938342b9e2e71184983243cb0be9b7
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-08-30 23:11:57 +0900

    refactor: update output

--- live/dev/outputs.tf
+++ live/dev/outputs.tf
@@ -1,11 +1,14 @@
-output "cluster_endpoint" {
-  description = "Endpoint for your Kubernetes API server"
-  value       = module.eks.cluster_endpoint
+output "vpc_id" {
+  description = "vpc id for your EKS Cluster"
+  value       = module.vpc.vpc_id
 }
-
 output "cluster_id" {
   description = "The name/id of the EKS cluster. Will block on cluster creation until the cluster is really ready"
   value       = module.eks.cluster_id
+}
+
+output "external_dns_role_arn" {
+    value = aws_iam_role.external_dns.arn
 }
 
 output "cluster_platform_version" {
