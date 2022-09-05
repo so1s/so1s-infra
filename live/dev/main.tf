636ed3807555e90e81e6ce1a7d49b1b98bcb99fe
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-05 18:31:09 +0900

    refactor: remove unnecessary code

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -1,9 +1,14 @@
+locals {
+  region = "ap-northeast-2"
+}
+
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-northeast-2a", "ap-northeast-2c", "ap-northeast-2b", "ap-northeast-2d"]
+  azs             = ["${local.region}a", "${local.region}c", "${local.region}b", "${local.region}d"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
@@ -59,19 +64,7 @@ module "eks" {
   subnet_ids = concat(module.vpc.private_subnets, module.vpc.public_subnets)
 
   cluster_security_group_name = "${var.global_name}-cluster-sg"
-  cluster_security_group_additional_rules = {
-    egress_nodes_ephemeral_ports_tcp = {
-      description                = "To node 1025-65535"
-      protocol                   = "tcp"
-      from_port                  = 1025
-      to_port                    = 65535
-      type                       = "egress"
-      source_node_security_group = true
-    }
-
-  }
-
-  node_security_group_name = "${var.global_name}-cluster-nodegroup-sg"
+  node_security_group_name    = "${var.global_name}-cluster-nodegroup-sg"
   node_security_group_additional_rules = {
     ingress_self_all = {
       description = "Node to node all ports/protocols"
@@ -81,16 +74,6 @@ module "eks" {
       type        = "ingress"
       self        = true
     }
-
-    ingress_cluster_api_ephemeral_ports_tcp = {
-      description                   = "Cluster API to K8S services running on nodes"
-      protocol                      = "tcp"
-      from_port                     = 1025
-      to_port                       = 65535
-      type                          = "ingress"
-      source_cluster_security_group = true
-    }
-
     egress_all = {
       description      = "Node all egress"
       protocol         = "-1"
@@ -118,10 +101,6 @@ module "eks" {
 
       create_iam_role = false
       iam_role_arn    = "arn:aws:iam::089143290485:role/So1s-data-plane-inference"
-
-      network_interfaces = [{
-        associate_public_ip_address = true
-      }]
 
       labels = {
         kind = "public"
--- live/dev/outputs.tf
+++ live/dev/outputs.tf
@@ -16,28 +16,12 @@ output "cluster_status" {
   description = "Status of the EKS cluster. One of `CREATING`, `ACTIVE`, `DELETING`, `FAILED`"
   value       = module.eks.cluster_status
 }
-
-output "cluster_primary_security_group_id" {
-  description = "Cluster security group that was created by Amazon EKS for the cluster. Managed node groups use this security group for control-plane-to-data-plane communication. Referred to as 'Cluster security group' in the EKS console"
-  value       = module.eks.cluster_primary_security_group_id
-}
-
 output "cluster_iam_role_name" {
   description = "IAM role name of the EKS cluster"
   value       = module.eks.cluster_iam_role_name
 }
 
-output "cluster_addons" {
-  description = "Map of attribute maps for all EKS cluster addons enabled"
-  value       = module.eks.cluster_addons
-}
-
 output "eks_managed_node_groups_autoscaling_group_names" {
   description = "List of the autoscaling group names created by EKS managed node groups"
   value       = module.eks.eks_managed_node_groups_autoscaling_group_names
-}
-
-output "aws_auth_configmap_yaml" {
-  description = "Formatted yaml output for base aws-auth configmap containing roles used in cluster node groups/fargate profiles"
-  value       = module.eks.aws_auth_configmap_yaml
 }
