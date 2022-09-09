014b869622b5d7c1b90931bf6decd17116a697e5
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-10 00:52:30 +0900

    refactor: remove unnecessary ouputs

--- live/prod/outputs.tf
+++ live/prod/outputs.tf
@@ -7,25 +7,6 @@ output "cluster_id" {
   value       = module.eks.cluster_id
 }
 
-output "cluster_platform_version" {
-  description = "Platform version for the cluster"
-  value       = module.eks.cluster_platform_version
-}
-
-output "cluster_status" {
-  description = "Status of the EKS cluster. One of `CREATING`, `ACTIVE`, `DELETING`, `FAILED`"
-  value       = module.eks.cluster_status
-}
-output "cluster_iam_role_name" {
-  description = "IAM role name of the EKS cluster"
-  value       = module.eks.cluster_iam_role_name
-}
-
-output "eks_managed_node_groups_autoscaling_group_names" {
-  description = "List of the autoscaling group names created by EKS managed node groups"
-  value       = module.eks.eks_managed_node_groups_autoscaling_group_names
-}
-
 output "external_dns_role_arn" {
   description = "External DNS ARN for external-dns chart values.yaml"
   value       = aws_iam_role.external_dns.arn
