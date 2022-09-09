7df6dc94b9ef9ecaaadd424c6ba1ec506eb031e9
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-09 17:29:33 +0900

    refactor: modify bootstrap.sh for prod

--- live/prod/bootstrap.sh
+++ live/prod/bootstrap.sh
@@ -5,14 +5,6 @@ if [ terraform != 0 ]; then
   echo "Your Terraform Version -> " `terraform version | head -n 1`
 fi
 
-# terraform variables check
-if [ -z $SO1S_GLOBAL_NAME ]; then
-  echo "Please Set global name using command 'export SO1S_GLOBAL_NAME=<GLOBAL_NAME>'"
-  exit 1
-else
-  echo "Complete Check global_name Variable -> $SO1S_GLOBAL_NAME"
-fi
-
 # Check Deploy Repositroy Path
 if [ -z $SO1S_DEPLOY_REPO_PATH ]; then
   echo "Please Set Deploy Repository Path using command 'export SO1S_DEPLOY_REPO_PATH=<DEPLOY_REPO_PATH>'"
@@ -23,33 +15,33 @@ fi
 
 echo -e "\n\n"
 echo "Terraform Initialize"
-echo "-> terraform init -backend-config=key=live/dev/$SO1S_GLOBAL_NAME"
-terraform init -backend-config="key=live/dev/$SO1S_GLOBAL_NAME"
+echo "-> terraform init"
+terraform init
 
 echo -e "\n"
 echo "Terraform Initialize"
-echo "-> terraform apply -var=global_name=$SO1S_GLOBAL_NAME"
-terraform apply -var="global_name=$SO1S_GLOBAL_NAME"
+echo "-> terraform apply"
+terraform apply
 
 echo -e "\n\n"
 echo "Update KubeConfig"
-echo "-> aws eks update-kubeconfig --region "ap-northeast-2" --name "$SO1S_GLOBAL_NAME-so1s-dev" --alias $SO1S_GLOBAL_NAME"
-aws eks update-kubeconfig --region "ap-northeast-2" --name "$SO1S_GLOBAL_NAME-so1s-dev" --alias $SO1S_GLOBAL_NAME
-kubectl config use-context $SO1S_GLOBAL_NAME
+echo "-> aws eks update-kubeconfig --region "ap-northeast-2" --name "prod-so1s" --alias prod-so1s"
+aws eks update-kubeconfig --region "ap-northeast-2" --name "prod-so1s" --alias prod-so1s
+kubectl config use-context prod-so1s
 
 echo -e "\n\n"
 
 # helm existing check
 if [ helm != 0 ]; then
-  echo "Your Helm Version -> " `helm version --short | head -n 1`
+  echo "Your Helm Version -> " `helm version --short | head -n 1`"
   echo -e "\n"
 fi
 
 # install argocd 
 echo "Install ArgoCD"
-echo "-> helm install argocd -n argocd -f $SO1S_DEPLOY_REPO_PATH/charts/argocd/argocd-dev-values.yaml argo/argo-cd --create-namespace --wait"
+echo "-> helm install argocd -n argocd -f $SO1S_DEPLOY_REPO_PATH/charts/argocd/argocd-prod-values.yaml argo/argo-cd --create-namespace --wait"
 helm repo add argo https://argoproj.github.io/argo-helm
-helm install argocd -n argocd -f $SO1S_DEPLOY_REPO_PATH/charts/argocd/argocd-dev-values.yaml argo/argo-cd --create-namespace --wait
+helm install argocd -n argocd -f $SO1S_DEPLOY_REPO_PATH/charts/argocd/argocd-prod-values.yaml argo/argo-cd --create-namespace --wait
 
 echo -e "\n\n"
 echo "ArgoCD Password -> " `kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
@@ -57,11 +49,11 @@ echo "ArgoCD Password -> " `kubectl -n argocd get secret argocd-initial-admin-se
 # create argocd project resource
 echo -e "\n\n"
 echo "Create ArgoCD Project Resource"
-echo "-> kubectl apply -f $SO1S_DEPLOY_REPO_PATH/project/project-dev.yaml"
-kubectl apply -f $SO1S_DEPLOY_REPO_PATH/project/project-dev.yaml --wait
+echo "-> kubectl apply -f $SO1S_DEPLOY_REPO_PATH/project/project-prod.yaml"
+kubectl apply -f $SO1S_DEPLOY_REPO_PATH/project/project-prod.yaml --wait
 
 # run root application
 echo -e "\n\n"
-echo "Run root-dev.yaml application"
-echo "-> kubectl apply -f $SO1S_DEPLOY_REPO_PATH/root-dev.yaml"
-kubectl apply -f $SO1S_DEPLOY_REPO_PATH/root-dev.yaml --wait
+echo "Run root-prod.yaml application"
+echo "-> kubectl apply -f $SO1S_DEPLOY_REPO_PATH/root-prod.yaml"
+kubectl apply -f $SO1S_DEPLOY_REPO_PATH/root-prod.yaml --wait
--- live/prod/variables.tf
+++ live/prod/variables.tf
@@ -1,5 +1,5 @@
 variable "global_name" {
   description = "This name will use as prefix for AWS resource so, Please write unique and distinct word e.g. sungbin, backend-test..."
   type        = string
-  default     = "production"
+  default     = "prod"
 }
