d40661f8e7dbd65a71961b9ea106173ffe2e84f9
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-10 16:54:56 +0900

    feat: add uninstall alb, external-dns

--- live/prod/clean-up.sh
+++ live/prod/clean-up.sh
@@ -1,6 +1,8 @@
 kubectl delete -f $SO1S_DEPLOY_REPO_PATH/root-prod.yaml --wait
 kubectl delete -f $SO1S_DEPLOY_REPO_PATH/project/project-prod.yaml --wait
 
-helm uninstall argocd -n argocd
+helm uninstall argocd -n argocd --wait
+helm uninstall external-dns -n kube-system --wait
+helm uninstall alb -n kube-system --wait 
 
 terraform destroy
