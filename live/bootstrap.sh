6ad77d83a974798a4248aacd9248ed477b55cdfb
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-12 15:28:02 +0900

    feat: complete integrate bootstrap

--- live/bootstrap.sh
+++ live/bootstrap.sh
@@ -1,59 +1,105 @@
 #!/bin/bash
 
-echo -e "어떤 환경으로 개발을 할 것인지 번호를 입력 해주세요. \n-> (1) prod (2) dev "
-read NUMBER
+SO1S_REGEX="^[1-2]$"
+while [[ ! $SO1S_ENV_NUMBER =~ $SO1S_REGEX ]]
+do
+  echo -e "어떤 환경으로 개발을 할 것인지 번호를 입력 해주세요. \n-> (1) prod (2) dev "
+  read SO1S_ENV_NUMBER
+done
 
-echo -e "Deploy Repository 경로를 입력 해주세요."
-read DEPLOY_REPO_PATH
-export $DEPLOY_REPO_PATH
+echo -e "\n"
 
-if [ -z $NUMBER ]; then
+SO1S_REGEX="^([.]{0,2}\/)*([A-z0-9-_+]+\/)*([A-z0-9-_]+)$"
+while [[ ! $SO1S_DEPLOY_REPO_PATH =~ $SO1S_REGEX ]]
+do
+  echo -e "Deploy Repository 경로를 입력 해주세요."
+  read SO1S_DEPLOY_REPO_PATH
+done
+
+# 환경에 따른 글로벌 이름 설정 -> Prod은 고정 값
+if [ $SO1S_ENV_NUMBER -eq 2 ]; then
+  echo -e "\n"
   echo "Terraform에 적용 할 GLOBAL_NAME을 설정해주세요."
-  read GLOBAL_NAME
-  export $GLOBAL_NAME
+  read SO1S_GLOBAL_NAME
+  # 추후 values.yaml 환경 지정을 위한 설정
+  SO1S_ENV_NAME="dev"
+elif [ $SO1S_ENV_NUMBER -eq 1 ]; then
+  SO1S_GLOBAL_NAME="prod"
+  # 추후 values.yaml 환경 지정을 위한 설정
+  SO1S_ENV_NAME="prod"
 fi
 
-
-if [ -z $NUMBER ]; then
-    echo -e "\n\n"
-    echo "Terraform Initialize"
-    echo "-> terraform init -backend-config=key=live/dev/$SO1S_GLOBAL_NAME"
-    terraform init -backend-config="key=live/dev/$SO1S_GLOBAL_NAME"
-
-    echo -e "\n"
-    echo "Terraform Initialize"
-    echo "-> terraform apply -var=global_name=$SO1S_GLOBAL_NAME"
-    terraform apply -var="global_name=$SO1S_GLOBAL_NAME"
-
-    echo -e "\n\n"
-    echo "Update KubeConfig"
-    echo "-> aws eks update-kubeconfig --region "ap-northeast-2" --name "$SO1S_GLOBAL_NAME-so1s-dev" --alias $SO1S_GLOBAL_NAME"
-    aws eks update-kubeconfig --region "ap-northeast-2" --name "$SO1S_GLOBAL_NAME-so1s-dev" --alias $SO1S_GLOBAL_NAME
-    kubectl config use-context $SO1S_GLOBAL_NAME
-else
-    echo -e "\n\n"
-    echo "Terraform Initialize"
-    echo "-> terraform init"
-    terraform init
-
-    echo -e "\n"
-    echo "Terraform Initialize"
-    echo "-> terraform apply"
-    terraform apply
-    RESULT=`terraform apply`
-    CLUSTER_NAME=`echo $RESULT | grep cluster_id | cut -d ' ' -f3`
-    echo $CLUSTER_NAME
-    VPC_ID=`echo $RESULT | grep vpc_id | cut -d ' ' -f3`
-    echo $VPC_ID
-    ROLE_ARN=`echo $RESULT | grep external_dns_role_arn | cut -d ' ' -f3`
-    echo $ROLE_ARN
-
-
-    echo -e "\n\n"
-    echo "Update KubeConfig"
-    echo "-> aws eks update-kubeconfig --region "ap-northeast-2" --name "prod-so1s" --alias prod-so1s"
-    aws eks update-kubeconfig --region "ap-northeast-2" --name "prod-so1s" --alias prod-so1s
-    kubectl config use-context prod-so1s
+# Check Terraform Version
+if [ terraform != 0 ]; then
+  echo "Your Terraform Version -> " `terraform version | head -n 1`
 fi
 
+echo -e "\n"
+echo "Terraform Initialize"
+if [ $SO1S_ENV_NUMBER -eq 1 ]; then
+  terraform init
+elif [ $SO1S_ENV_NUMBER -eq 2 ]; then
+  echo "-> terraform init -backend-config=key=live/dev/$SO1S_GLOBAL_NAME"
+  terraform init -backend-config="key=live/dev/$SO1S_GLOBAL_NAME"
+fi
 
+echo -e "\n"
+echo "Start Resource Provisioning"
+echo "-> terraform apply -var=global_name=$SO1S_GLOBAL_NAME"
+terraform apply -var="global_name=$SO1S_GLOBAL_NAME"
+
+# Using for ALB, External DNS Chart
+RESULT=`terraform apply -var="global_name=$SO1S_GLOBAL_NAME"`
+CLUSTER_NAME=`echo $RESULT | grep cluster_id | cut -d ' ' -f3`
+echo $CLUSTER_NAME
+VPC_ID=`echo $RESULT | grep vpc_id | cut -d ' ' -f3`
+echo $VPC_ID
+ROLE_ARN=`echo $RESULT | grep external_dns_role_arn | cut -d ' ' -f3`
+echo $ROLE_ARN
+
+[[ $SO1S_ENV_NUMBER = 1 ]] && SO1S_CLUSTER_NAME="$SO1S_GLOBAL_NAME-so1s" || SO1S_CLUSTER_NAME="$SO1S_GLOBAL_NAME-so1s-dev"
+
+echo -e "\n"
+echo "Update KubeConfig"
+echo "-> aws eks update-kubeconfig --region "ap-northeast-2" --name "$SO1S_CLUSTER_NAME" --alias $SO1S_GLOBAL_NAME"
+aws eks update-kubeconfig --region "ap-northeast-2" --name "$SO1S_CLUSTER_NAME" --alias $SO1S_GLOBAL_NAME
+kubectl config use-context $SO1S_GLOBAL_NAME
+
+# Check Helm Version
+if [ helm != 0 ]; then
+  echo -e "\n"
+  echo "Your Helm Version -> " `helm version --short | head -n 1`
+fi
+
+if [ $SO1S_ENV_NUMBER -eq 1 ]; then
+  # install alb chart
+  echo -e "\n"
+  echo "Install ALB"
+  helm install alb -n kube-system -f $SO1S_DEPLOY_REPO_PATH/charts/public/aws-load-balancer-controller/dev-values.yaml eks/aws-load-balancer-controller --create-namespace --wait --set clusterName=$CLUSTER_NAME --set vpcId=$VPC_ID
+
+  # install external-dns chart
+  echo "Install external-dns"
+  helm install external-dns -n kube-system -f $SO1S_DEPLOY_REPO_PATH/charts/public/external-dns/dev-values.yaml $SO1S_DEPLOY_REPO_PATH/charts/public/external-dns --create-namespace --wait --set serviceAccount.roleArn=$ROLE_ARN
+fi
+
+# install argocd 
+echo -e "\n"
+echo "Install ArgoCD"
+echo "-> helm install argocd -n argocd -f $SO1S_DEPLOY_REPO_PATH/charts/argocd/argocd-$SO1S_ENV_NAME-values.yaml argo/argo-cd --create-namespace --wait"
+helm repo add argo https://argoproj.github.io/argo-helm
+helm install argocd -n argocd -f $SO1S_DEPLOY_REPO_PATH/charts/argocd/argocd-$SO1S_ENV_NAME-values.yaml argo/argo-cd --create-namespace --wait
+
+echo -e "\n\n"
+echo "ArgoCD Password -> " `kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
+
+# Create argocd project resource
+echo -e "\n"
+echo "Create ArgoCD Project Resource"
+echo "-> kubectl apply -f $SO1S_DEPLOY_REPO_PATH/project/project-$SO1S_ENV_NAME.yaml"
+kubectl apply -f $SO1S_DEPLOY_REPO_PATH/project/project-$SO1S_ENV_NAME.yaml --wait
+
+# Run root application
+echo -e "\n"
+echo "Run root-$SO1S_ENV_NAME.yaml application"
+echo "-> kubectl apply -f $SO1S_DEPLOY_REPO_PATH/root-$SO1S_ENV_NAME.yaml"
+kubectl apply -f $SO1S_DEPLOY_REPO_PATH/root-$SO1S_ENV_NAME.yaml --wait
