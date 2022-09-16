3a94cda0cd9225291b43abc1e9e11668470ddfb4
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-16 17:09:12 +0900

    feat: add output in cluster

--- modules/cluster/outputs.tf
+++ modules/cluster/outputs.tf
@@ -2,3 +2,15 @@ output "cluster_id" {
   description = "The name/id of the EKS cluster. Will block on cluster creation until the cluster is really ready"
   value       = module.eks.cluster_id
 }
+
+output "cluster_oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "cluster_oidc_issuer_url" {
+  value = module.eks.cluster_oidc_issuer_url
+}
+
+output "cluster_oidc_provider" {
+  value = module.eks.oidc_provider
+}
