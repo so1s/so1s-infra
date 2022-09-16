fce1191e0b6131c9212feb1682e4214f7ed051a0
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-16 17:09:01 +0900

    feat: add variable in vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,17 +1,13 @@
-locals {
-  region = "ap-northeast-2"
-}
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  cidr = "10.0.0.0/16"
+  cidr = var.cidr
 
-  azs             = ["${local.region}a", "${local.region}c", "${local.region}b", "${local.region}d"]
+  azs             = ["${var.region}a", "${var.region}c", "${var.region}b", "${var.region}d"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway     = true
+  enable_nat_gateway     = var.enable_nat_gateway
   single_nat_gateway     = true
   one_nat_gateway_per_az = false
 
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  description = "This type will use vpc region setting."
+  type        = string
+}
+
 variable "is_prod" {
   description = "This type will check deploy type."
   type        = bool
@@ -6,4 +11,28 @@ variable "is_prod" {
 variable "global_name" {
   description = "This name will use as prefix for AWS resource so, Please write unique and distinct word e.g. sungbin, backend-test..."
   type        = string
+}
+
+variable "cidr" {
+  description = "This type will use to set cidr"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "This type will use to set public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "This type will use to set private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "This type will check whether to use NAT Gateway or not"
+  type        = bool
+  default     = true
 }
