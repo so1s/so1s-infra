42e7646b1cc75b1e102ac537a9390e52c06d1607
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-10-25 17:16:59 +0900

    feat: reflect gpu builder setting

--- live/dev/variables.tf
+++ live/dev/variables.tf
@@ -1,10 +1,16 @@
 variable "global_name" {
   description = "This name will use as prefix for AWS resource so, Please write unique and distinct word e.g. sungbin, backend-test..."
-  type = string
+  type        = string
 }
 
 variable "inference_node_instance_types" {
   description = "This type will use inference node that use eks"
   type        = list(string)
   default     = ["t3a.large"]
+}
+
+variable "model_builder_node_instance_types" {
+  description = "This type will use inference node that use eks"
+  type        = list(string)
+  default     = ["r6a.large"]
 }
