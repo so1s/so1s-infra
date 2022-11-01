ad7bf239bd3de2b2b206f0882aba5a0401c6a83d
Jiho Lee <optional.int@kakao.com>
2022-11-01 12:25:35 +0900

    refactor: remove comment

--- live/bootstrap-sealed-secret.sh
+++ live/bootstrap-sealed-secret.sh
@@ -13,7 +13,6 @@ echo "Inject Sealed Secret Certificate"
 echo "-> kubectl apply -f $SO1S_DEPLOY_REPO_PATH/cert.yaml"
 kubectl apply -f $SO1S_DEPLOY_REPO_PATH/cert.yaml --wait
 kubectl rollout restart deployment -n sealed-secrets so1s-sealed-secrets
-# IMAGE PULL ERROR난 backend deployment의 Replicas를 지우고 다시 생성한다.
 echo "Wait for Sealed-Secret to be created"
 sleep 10
 DEPLOYMENT_NAME=`kubectl get deployment -n backend | grep so1s | cut -d ' ' -f1`
