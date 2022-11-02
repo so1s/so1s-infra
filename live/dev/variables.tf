f093fb55992d9d091e07d2f6ebf4c6747d5b13e3
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-11-02 16:39:41 +0900

    fix: chagne model-builder spec

--- live/dev/variables.tf
+++ live/dev/variables.tf
@@ -12,5 +12,5 @@ variable "inference_node_instance_types" {
 variable "model_builder_node_instance_types" {
   description = "This type will use inference node that use eks"
   type        = list(string)
-  default     = ["r6a.large"]
+  default     = ["r5a.large"]
 }
